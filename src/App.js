79256a6846476c24acf24ccc1509538bafb5fb7c
ar25029 <ar25029@gmail.com>
2025-10-29 21:50:46 +0530

    small update in code with config and app modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// src/App.jsx
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CreatorProvider } from "./contexts/CreatorContext";
 import Dashboard from "./pages/Dashboard";
